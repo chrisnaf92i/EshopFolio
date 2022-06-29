7283c0f6f1b4da75c965d285a7f234cd046986df
christopher nafrere <nafrere.christopher@gmail.com>
2022-06-29 15:39:24 +0200

    Préparation des routes d'authentification

---
+++ router.js
@@ -0,0 +1,13 @@
+import express from "express";
+import { signIn } from "./controller/auth.js";
+
+const Router = express.Router();
+
+Router.get("/", (req, res) => {
+    res.json({"message": "bienvenu sur l'api de MyEshopFolio"});
+});
+
+Router.get("/sign-in", signIn);
+Router.get("/login", login);
+
+export default Router;
--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
+import Router from "./router.js";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 8000;
 
 const App = express();
+
+App.use(express.json());
+
+App.use(Router);
 
 App.listen(PORT, console.log(`Lancement du server sur le port ${PORT}`));
